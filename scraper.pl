15bfded211ea9bf29c3ca80df5837753456cb3c1
Mikhail Zavoloka <mikhail.zavoloka@gmail.com>
2015-03-31 22:04:09 +0300

    Parsing 2nd page

--- scraper.pl
+++ scraper.pl
@@ -7,7 +7,7 @@ use URI::Escape qw( uri_escape );
 
 my $path_to_file_with_queries = $ARGV[ 0 ];
 my $async = HTTP::Async -> new();
-my $link = "google.com/search?q=";
+my $url = "google.com/search?q=";
 
 if ( not $path_to_file_with_queries )
 {
@@ -23,7 +23,7 @@ else
     open( my $fh, '<', $path_to_file_with_queries )
         or die "Couldn't open file $path_to_file_with_queries: $!";
 
-    $async -> add( map { HTTP::Request -> new( $link . uri_escape( $_ ) ) } <$fh> );
+    $async -> add( map { HTTP::Request -> new( $url . uri_escape( $_ ) ) } <$fh> );
 
     close( $fh );
 
@@ -40,31 +40,39 @@ sub parse
 
     my $tree = HTML::TreeBuilder -> new_from_content( $response -> content() );
 
-    my @top10_divs = $tree -> look_down( _tag => 'div', class => 'rc' );
-    my @top10_info = ();
+    my $parsing_first_page = response -> base() !~ /&start=10$/;
 
-    my $i = 1;
-    for my $div ( @top10_divs )
+    if( $parsing_first_page )
     {
-        my $a = $div -> look_down( _tag => 'h3', 'class' => 'r' ) -> look_down( _tag => 'a' );
+        my @top10_divs = $tree -> look_down( _tag => 'div', class => 'rc' );
+        my $rank = 1;
+        my @top10_info = map { &get_info( $_, $rank ++ ) } @top10_divs;
 
-        my $info = {
-            number      => $i++,
-            url         => $a -> attr( 'href' ),
-            title       => $a -> as_text(),
-            description => $div -> look_down( _tag => 'span', 'class' => 'st' ) -> as_text()
-        };
-
-        push( @top10_info, $info );
-    }
-
-    # There may be only 9 results on the first page (example query: "obama"). I think, It's due to images bar.
-    if( @top10_divs < 10 )
-    {
-        my $second_page_exists = $tree -> look_down( _tag => 'table', id => 'nav' );
-        if( $second_page_exists )
+        # There may be only 9 results on the first page (example query: "obama"). I think, it's due to images bar.
+        if( @top10_divs < 10 )
         {
-            $async -> add( HTTP::Request -> new( $response -> base() . '&start=10' ) );
+            my $second_page_exists = $tree -> look_down( _tag => 'table', id => 'nav' );
+            if( $second_page_exists )
+            {
+                $async -> add( HTTP::Request -> new( $response -> base() . '&start=10' ) );
+            }
         }
     }
+    else # parsing second page
+    {
+        my $one_more_div = $tree -> look_down( _tag => 'div', class => 'rc' );
+    }
+}
+
+sub get_info
+{
+    my ( $div, $rank ) = @_;
+    my $a = $div -> look_down( _tag => 'h3', 'class' => 'r' ) -> look_down( _tag => 'a' );
+
+    return {
+        rank        => $rank,
+        url         => $a -> attr( 'href' ),
+        title       => $a -> as_text(),
+        description => $div -> look_down( _tag => 'span', 'class' => 'st' ) -> as_text()
+    };
 }
